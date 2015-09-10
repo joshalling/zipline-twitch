e217f61fdb85b9f4febab3f9d820965cfca404a4
Josh Alling <jalling90@gmail.com>
2015-09-10 07:17:36 -0600

    added html and js

---
+++ app.js
@@ -0,0 +1,89 @@
+angular.module('twitchApp', [])
+  .controller('streamerCtrl', ['$scope', '$http', function($scope, $http) {
+    
+    // Scope variables. streams will hold all the data, while streamsDisplay will be filtered based on user interation.
+    $scope.streams = [];
+    $scope.streamsDisplay = [];
+    $scope.streamsTemp = [];
+    
+    // Local variables
+    var streamers = ["freecodecamp", "medrybw", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff",];
+    
+    // Functions
+    function moveSelected(add, remove) {
+      $(remove).removeClass('selected');
+      if (!$(add).hasClass('selected')) {
+        $(add).addClass('selected')
+      }
+    }
+    
+    // Scope functions
+    
+    $scope.allClick = function() {
+      moveSelected('#all', '#online, #offline');
+      
+      $scope.streamsDisplay = $scope.streams.slice(0);
+      $scope.streamsTemp = $scope.streamsDisplay.slice(0);
+    }
+    
+    $scope.onlineClick = function() {
+      moveSelected('#online', '#all, #offline');
+
+      $scope.streamsDisplay = $scope.streams.filter(function(stream) {
+        return stream.status === 'online';
+      });
+      $scope.streamsTemp = $scope.streamsDisplay.slice(0);
+    }
+    
+    $scope.offlineClick = function() {
+      moveSelected('#offline', '#online, #all');
+
+      $scope.streamsDisplay = $scope.streams.filter(function(stream) {
+        return stream.status === 'offline';
+      });
+      $scope.streamsTemp = $scope.streamsDisplay.slice(0);
+    }
+    
+    $scope.performSearch = function() {
+      $scope.streamsDisplay = $scope.streamsTemp.filter(function(stream) {
+        return stream.name.match(new RegExp($('#search').val(), 'i'));
+      });
+    }
+    
+    // Get stream and channel info for each streamer
+    streamers.forEach(function(streamer) {
+      $http.jsonp('https://api.twitch.tv/kraken/streams/' + streamer + '?callback=JSON_CALLBACK')
+        .success(function(stream) {
+          $http.jsonp(stream._links.channel + '?callback=JSON_CALLBACK')
+            .success(function(channel) {
+              // create new object to hold the info that will be used in this app
+              var pushStream = {};
+              
+              // Set status based on whether the stream is online or not
+              if (stream.stream) {
+                pushStream.status = 'online';
+                pushStream.description = channel.status.slice(0,25) + '...';
+              } else {
+                pushStream.status = 'offline';
+              }
+            
+              // Assign channel logo url if there is one. Otherwise use a generic avatar
+              if (channel.logo) {
+                pushStream.logo = channel.logo;
+              } else {
+                pushStream.logo = 'http://static-cdn.jtvnw.net/jtv-static/404_preview-300x300.png';
+              }
+            
+              // Assign other properties to the stream
+              pushStream.id = channel.name;
+              pushStream.name = channel.display_name;
+              pushStream.url = channel.url;
+              
+              // Push the obj into $scope streams arrays
+              $scope.streams.push(pushStream);
+              $scope.streamsDisplay.push(pushStream);
+              $scope.streamsTemp.push(pushStream);
+            });
+        });
+    });    
+  }]);
